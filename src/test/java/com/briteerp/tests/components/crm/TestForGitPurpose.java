224360c4831021f7e2e87129fddb75ab903cbb9c
mehmetdurkan <mehmet_ durkan@icloud.com>
2019-08-03 13:56:16 -0500

    Commit-3 added before method

--- src/test/java/com/briteerp/tests/components/crm/TestForGitPurpose.java
+++ src/test/java/com/briteerp/tests/components/crm/TestForGitPurpose.java
@@ -1,4 +1,12 @@
 package com.briteerp.tests.components.crm;
 
+import org.testng.annotations.BeforeMethod;
+
 public class TestForGitPurpose {
+
+    @BeforeMethod
+    public void setUp(){
+
+
+    }
 }
